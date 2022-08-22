95a46a7c4d87823df313a865ddbe6426bfce1e49
Lenovo <benasvol@gmail.com>
2022-08-22 16:26:02 +0300

    IPF added custom errors, updated tests.

--- packages/investment-pool/contracts/mock/GelatoOpsMock.sol
+++ packages/investment-pool/contracts/mock/GelatoOpsMock.sol
@@ -9,21 +9,21 @@ contract GelatoOpsMock is IGelatoOps {
     event RegisterGelatoTask();
 
     function createTaskNoPrepayment(
-        address _execAddress,
-        bytes4 _execSelector,
-        address _resolverAddress,
-        bytes calldata _resolverData,
-        address _feeToken
+        address, /*_execAddress*/
+        bytes4, /*_execSelector*/
+        address, /*_resolverAddress*/
+        bytes calldata, /*_resolverData*/
+        address /*_feeToken*/
     ) public returns (bytes32 task) {
         emit RegisterGelatoTask();
         task = bytes32("");
     }
 
-    function getFeeDetails() public view returns (uint256, address) {
+    function getFeeDetails() public pure returns (uint256, address) {
         return (uint256(0), address(0));
     }
 
-    function gelato() public view returns (address payable) {
+    function gelato() public pure returns (address payable) {
         return (payable(address(0)));
     }
 }
--- packages/investment-pool/hardhat.config.ts
+++ packages/investment-pool/hardhat.config.ts
@@ -1,7 +1,7 @@
 import * as dotenv from "dotenv";
 import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomicfoundation/hardhat-chai-matchers";
 import "@nomiclabs/hardhat-etherscan";
-import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-web3";
 import "@typechain/hardhat";
