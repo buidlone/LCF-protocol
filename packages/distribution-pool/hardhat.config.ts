6d033b5541cd729684cd4e32e4b39fc9355a3074
Lenovo <benasvol@gmail.com>
2022-11-16 18:35:21 +0200

    Updated DP hardhat config file

--- packages/distribution-pool/hardhat.config.ts
+++ packages/distribution-pool/hardhat.config.ts
@@ -1,8 +1,52 @@
-import { HardhatUserConfig } from "hardhat/config";
+import {HardhatUserConfig, task} from "hardhat/config";
+import "dotenv/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-web3";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+import "hardhat-deploy";
+import "hardhat-docgen";
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+    solidity: {
+        version: "0.8.14",
+        settings: {
+            optimizer: {
+                enabled: true,
+            },
+        },
+    },
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+        },
+        goerli: {
+            url: process.env.GOERLI_URL || "",
+            accounts:
+                process.env.DEPLOYER_PRIVATE_KEY !== undefined
+                    ? [process.env.DEPLOYER_PRIVATE_KEY]
+                    : [],
+            chainId: 5,
+        },
+    },
+    gasReporter: {
+        enabled: process.env.REPORT_GAS !== undefined,
+        currency: "EUR",
+    },
+    etherscan: {
+        apiKey: process.env.ETHERSCAN_API_KEY,
+    },
+    docgen: {
+        path: "./docs",
+        clear: true,
+        runOnCompile: false,
+    },
+    mocha: {
+        timeout: 200000,
+    },
 };
 
 export default config;
