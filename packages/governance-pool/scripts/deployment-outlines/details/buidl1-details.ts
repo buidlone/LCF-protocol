a5f999fa8d7f6589786cd8a25e339558d0955843
Lenovo <benasvol@gmail.com>
2022-12-20 18:06:18 +0200

    Updated MVP buidl1 project details

--- packages/governance-pool/scripts/deployment-outlines/details/buidl1-details.ts
+++ packages/governance-pool/scripts/deployment-outlines/details/buidl1-details.ts
@@ -2,78 +2,82 @@ import {ethers, network} from "hardhat";
 import {BigNumber} from "ethers";
 import {formatPercentage} from "../../format-percentage";
 
+const getTimestamp = (time: string): number => {
+    return new Date(time).getTime() / 1000;
+};
+
 export const buidl1Details = async (): Promise<
     [BigNumber, BigNumber, BigNumber, BigNumber, number, number, any, string]
 > => {
     if (network.name != "goerli") {
         throw "Network is not goerli!";
     } else {
-        const acceptedSuperToken = "0x8aE68021f6170E5a766bE613cEA0d75236ECCa9a"; // fUSDCx
-        const softCap: BigNumber = ethers.utils.parseEther("135000");
-        const hardCap: BigNumber = ethers.utils.parseEther("565000");
+        const acceptedSuperToken = "0x5943F705aBb6834Cad767e6E4bB258Bc48D9C947"; // ETHx
+        const softCap: BigNumber = ethers.utils.parseEther("25");
+        const hardCap: BigNumber = ethers.utils.parseEther("65");
         const gelatoFeeAllocation: BigNumber = ethers.utils.parseEther("0.1");
         const tokenRewards: BigNumber = ethers.utils.parseEther("15000000");
-        const fundraiserStartDate: number = 1671616800; // Dec 21, 2022 12:00
-        const fundraiserEndDate: number = 1677578400; // Feb 28, 2023 12:00
+        const fundraiserStartDate: number = getTimestamp("2022-12-21T14:00:00");
+        const fundraiserEndDate: number = getTimestamp("2023-02-28T12:00:00");
 
         const milestones = [
             {
-                startDate: 1677664800, //  March 1, 2023 12:00:00 PM GMT+02:00
-                endDate: 1682931600, // May 1, 2023 12:00:00 PM GMT+03:00
+                startDate: getTimestamp("2023-03-01T12:00:00"),
+                endDate: getTimestamp("2023-05-01T12:00:00"),
                 intervalSeedPortion: formatPercentage(0.5),
                 intervalStreamingPortion: formatPercentage(6.5),
             },
             {
-                startDate: 1682931600, // May 1, 2023 12:00:00 PM GMT+03:00
-                endDate: 1688202000, // July 1, 2023 12:00:00 PM GMT+03:00
+                startDate: getTimestamp("2023-05-01T12:00:00"),
+                endDate: getTimestamp("2023-07-01T12:00:00"),
                 intervalSeedPortion: formatPercentage(0.5),
                 intervalStreamingPortion: formatPercentage(7),
             },
             {
-                startDate: 1688202000, // July 1, 2023 12:00:00 PM GMT+03:00
-                endDate: 1693558800, // September 1, 2023 12:00:00 PM GMT+03:00
+                startDate: getTimestamp("2023-07-01T12:00:00"),
+                endDate: getTimestamp("2023-09-01T12:00:00"),
                 intervalSeedPortion: formatPercentage(0.5),
                 intervalStreamingPortion: formatPercentage(7),
             },
             {
-                startDate: 1693558800, // September 1, 2023 12:00:00 PM GMT+03:00
-                endDate: 1698832800, // November 1, 2023 12:00:00 PM GMT+02:00
+                startDate: getTimestamp("2023-09-01T12:00:00"),
+                endDate: getTimestamp("2023-11-01T12:00:00"),
                 intervalSeedPortion: formatPercentage(0.5),
                 intervalStreamingPortion: formatPercentage(8),
             },
             {
-                startDate: 1698832800, // November 1, 2023 12:00:00 PM GMT+02:00
-                endDate: 1704103200, // January 1, 2024 12:00:00 PM GMT+02:00
+                startDate: getTimestamp("2023-11-01T12:00:00"),
+                endDate: getTimestamp("2024-01-01T12:00:00"),
                 intervalSeedPortion: formatPercentage(0.5),
                 intervalStreamingPortion: formatPercentage(8.5),
             },
             {
-                startDate: 1704103200, // January 1, 2024 12:00:00 PM GMT+02:00
-                endDate: 1709287200, // March 1, 2024 12:00:00 PM GMT+02:00
+                startDate: getTimestamp("2024-01-01T12:00:00"),
+                endDate: getTimestamp("2024-03-01T12:00:00"),
                 intervalSeedPortion: formatPercentage(1),
                 intervalStreamingPortion: formatPercentage(9),
             },
             {
-                startDate: 1709287200, // March 1, 2024 12:00:00 PM GMT+02:00
-                endDate: 1714554000, // May 1, 2024 12:00:00 PM GMT+03:00
+                startDate: getTimestamp("2024-03-01T12:00:00"),
+                endDate: getTimestamp("2024-05-01T12:00:00"),
                 intervalSeedPortion: formatPercentage(0.5),
                 intervalStreamingPortion: formatPercentage(9.5),
             },
             {
-                startDate: 1714554000, // May 1, 2024 12:00:00 PM GMT+03:00
-                endDate: 1719824400, // July 1, 2024 12:00:00 PM GMT+03:00
+                startDate: getTimestamp("2024-05-01T12:00:00"),
+                endDate: getTimestamp("2024-07-01T12:00:00"),
                 intervalSeedPortion: formatPercentage(0.5),
                 intervalStreamingPortion: formatPercentage(10),
             },
             {
-                startDate: 1719824400, // July 1, 2024 12:00:00 PM GMT+03:00
-                endDate: 1725181200, // September 1, 2024 12:00:00 PM GMT+03:00
+                startDate: getTimestamp("2024-07-01T12:00:00"),
+                endDate: getTimestamp("2024-09-01T12:00:00"),
                 intervalSeedPortion: formatPercentage(1),
                 intervalStreamingPortion: formatPercentage(13),
             },
             {
-                startDate: 1725181200, // September 1, 2024 12:00:00 PM GMT+03:00
-                endDate: 1730455200, // November 1, 2024 12:00:00 PM GMT+02:00
+                startDate: getTimestamp("2024-09-01T12:00:00"),
+                endDate: getTimestamp("2024-11-01T12:00:00"),
                 intervalSeedPortion: formatPercentage(0),
                 intervalStreamingPortion: formatPercentage(16),
             },
